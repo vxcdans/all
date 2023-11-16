123d226618d94ccb9dab5a3bbb75e21ddf7cd0e2
DANTE <104479517+xflydan@users.noreply.github.com>
2023-11-16 11:50:32 +0700

    Update pler.py

--- pler.py
+++ pler.py
@@ -28,7 +28,7 @@ emoji = "😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 �
 )
 
 
-@kntl.on(events.NewMessage(pattern="^/start$"))
+@kntl.on(events.NewMessage(pattern="^/s$"))
 async def help(event):
   helptext = "**Ada 2 Mode Tag All Cok, Kalo /all random emote tanpa nama. kalo /emojitag itu user emot sange + nama user.**"
   await event.reply(
