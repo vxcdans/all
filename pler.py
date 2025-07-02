e00786b67e9e3ddde46e792bcbd1073c4223ed74
DANTE <104479517+vxcdans@users.noreply.github.com>
2025-07-02 16:41:27 +0700

    Update pler.py

--- pler.py
+++ pler.py
@@ -93,7 +93,7 @@ async def mentionall(event):
     if not chat_id in spam_chats:
       break
     usrnum += 1
-    usrtxt += f"🥵 [{usr.first_name}](tg://user?id={usr.id})\n"
+    usrtxt += f"🤖 [{usr.first_name}](tg://user?id={usr.id})\n"
     if usrnum == 5:
       if mode == "teks":
         txt = f"{usrtxt}\n\n{msg}"
